ffc902e6341eb9bbfd87b782d748c13165163929
Marin Hannache <git@mareo.fr>
2022-06-03 20:40:27 +0200

    wip
    Signed-off-by: Marin Hannache <git@mareo.fr>

--- vault/k8s.mareo.fr/nextcloud.tf
+++ vault/k8s.mareo.fr/nextcloud.tf
@@ -1,14 +1,28 @@
 resource "random_password" "nextcloud_password" {
-  length           = 16
+  length           = 32
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
 resource "vault_generic_secret" "nextcloud" {
-  path         = "k8s/nextcloud"
+  path         = "k8s/nextcloud/initial-account-credentials"
   disable_read = true
   data_json = jsonencode({
     username = "admin"
     password = random_password.nextcloud_password.result
+  })
+}
+
+resource "random_password" "nextcloud_redis-password" {
+  length           = 32
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "vault_generic_secret" "nextcloud_redis-password" {
+  path         = "k8s/nextcloud/redis"
+  disable_read = true
+  data_json = jsonencode({
+    password = random_password.nextcloud_redis-password.result
   })
 }
