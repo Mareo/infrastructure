f2d4c97027532f06e2ad1627ff99433e14fef9dd
Marin Hannache <git@mareo.fr>
2022-06-03 20:28:10 +0200

    wip
    Signed-off-by: Marin Hannache <git@mareo.fr>

--- vault/k8s.mareo.fr/gitlab.tf
+++ vault/k8s.mareo.fr/gitlab.tf
@@ -1,5 +1,5 @@
 resource "random_password" "gitlab_initial-root-password" {
-  length           = 16
+  length           = 32
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
@@ -9,5 +9,19 @@ resource "vault_generic_secret" "gitlab_initial-root-password" {
   disable_read = true
   data_json = jsonencode({
     password = random_password.gitlab_initial-root-password.result
+  })
+}
+
+resource "random_password" "gitlab_redis-password" {
+  length           = 32
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "vault_generic_secret" "gitlab_redis-password" {
+  path         = "k8s/gitlab/redis"
+  disable_read = true
+  data_json = jsonencode({
+    password = random_password.gitlab_redis-password.result
   })
 }
