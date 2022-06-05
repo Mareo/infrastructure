a66da49ae8a8360135131351d43b3530a74a8713
Marin Hannache <git@mareo.fr>
2022-06-05 02:50:32 +0200

    wip
    Signed-off-by: Marin Hannache <git@mareo.fr>

--- vault/k8s.mareo.fr/authentik.tf
+++ vault/k8s.mareo.fr/authentik.tf
@@ -4,11 +4,18 @@ resource "random_password" "authentik_secret-key" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
+resource "random_password" "authentik_bootstrap-password" {
+  length           = 32
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
 resource "vault_generic_secret" "authentik" {
   path         = "k8s/authentik/app"
   disable_read = true
   data_json = jsonencode({
-    secret_key = random_password.authentik_secret-key.result
+    secret_key         = random_password.authentik_secret-key.result
+    bootstrap_password = random_password.authentik_bootstrap-password.result
   })
 }
 
