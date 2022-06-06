07cf56dadcd8300d401f285b9485270269e3a954
Marin Hannache <git@mareo.fr>
2022-06-06 12:43:55 +0200

    wip
    Signed-off-by: Marin Hannache <git@mareo.fr>

--- authentik/gitlab-oidc.tf
+++ authentik/gitlab-oidc.tf
@@ -1,9 +1,9 @@
-resource "random_password" "gitlab_client-id" {
+resource "random_password" "gitlab-oidc_client-id" {
   length  = 32
   special = false
 }
 
-resource "random_password" "gitlab_client-secret" {
+resource "random_password" "gitlab-oidc_client-secret" {
   length  = 64
   special = false
 }
@@ -12,16 +12,16 @@ resource "vault_generic_secret" "gitlab_oidc-authentik" {
   path         = "k8s/gitlab/oidc-authentik"
   disable_read = true
   data_json = jsonencode({
-    client_id     = random_password.gitlab_client-id.result
-    client_secret = random_password.gitlab_client-secret.result
+    client_id     = random_password.gitlab-oidc_client-id.result
+    client_secret = random_password.gitlab-oidc_client-secret.result
   })
 }
 
-resource "authentik_provider_oauth2" "gitlab" {
-  name               = "gitlab"
+resource "authentik_provider_oauth2" "gitlab-oidc" {
+  name               = "gitlab-oidc"
   authorization_flow = data.authentik_flow.default-provider-authorization-implicit-consent.id
-  client_id          = random_password.gitlab_client-id.result
-  client_secret      = random_password.gitlab_client-secret.result
+  client_id          = random_password.gitlab-oidc_client-id.result
+  client_secret      = random_password.gitlab-oidc_client-secret.result
   redirect_uris = [
     "https://gitlab.mareo.fr/users/auth/openid_connect/callback"
   ]
@@ -33,23 +33,23 @@ resource "authentik_provider_oauth2" "gitlab" {
   signing_key = data.authentik_certificate_key_pair.default.id
 }
 
-resource "authentik_application" "gitlab" {
-  name               = "GitLab"
-  slug               = "gitlab"
+resource "authentik_application" "gitlab-oidc" {
+  name               = "GitLab (OIDC)"
+  slug               = "gitlab-oidc"
   group              = "Services"
-  protocol_provider  = authentik_provider_oauth2.gitlab.id
+  protocol_provider  = authentik_provider_oauth2.gitlab-oidc.id
   meta_icon          = "https://about.gitlab.com/images/press/press-kit-icon.svg"
   meta_launch_url    = "https://gitlab.mareo.fr/"
   meta_publisher     = "GitLab Inc."
   policy_engine_mode = "any"
 }
 
-resource "authentik_policy_binding" "gitlab_group-filtering" {
+resource "authentik_policy_binding" "gitlab-oidc_group-filtering" {
   for_each = { for idx, value in [
     "gitlab",
     "gitlab_admins",
   ] : idx => value }
-  target = authentik_application.gitlab.uuid
+  target = authentik_application.gitlab-oidc.uuid
   group  = authentik_group.groups[each.value].id
   order  = each.key
 }
