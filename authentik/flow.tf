7d1a7c319d8ca1b24b1c12140730bc63e1436141
Marin Hannache <git@mareo.fr>
2022-06-06 12:59:14 +0200

    wip
    Signed-off-by: Marin Hannache <git@mareo.fr>

--- authentik/flow.tf
+++
@@ -1,7 +0,0 @@
-data "authentik_flow" "default-provider-authorization-implicit-consent" {
-  slug = "default-provider-authorization-implicit-consent"
-}
-
-data "authentik_flow" "default-provider-authorization-explicit-consent" {
-  slug = "default-provider-authorization-explicit-consent"
-}
