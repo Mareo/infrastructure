f14b3aa3b4780c30efc958e571105682894d5cfc
Marin Hannache <mareo@cri.epita.fr>
2022-06-11 19:58:03 +0200

    wip
    Signed-off-by: Marin Hannache <mareo@cri.epita.fr>

--- authentik/argocd.tf
+++ authentik/argocd.tf
@@ -3,17 +3,11 @@ resource "random_string" "argocd_client-id" {
   special = false
 }
 
-resource "random_password" "argocd_client-secret" {
-  length  = 64
-  special = false
-}
-
 resource "vault_generic_secret" "argocd_oidc-authentik" {
   path         = "k8s/argocd/oidc-authentik"
   disable_read = true
   data_json = jsonencode({
     client_id     = random_string.argocd_client-id.result
-    client_secret = random_password.argocd_client-secret.result
   })
 }
 
@@ -21,10 +15,11 @@ resource "authentik_provider_oauth2" "argocd" {
   name               = "argocd"
   authorization_flow = data.authentik_flow.default-provider-authorization-implicit-consent.id
   client_id          = random_string.argocd_client-id.result
-  client_secret      = random_password.argocd_client-secret.result
+  client_type        = "public"
   token_validity     = "days=1"
   redirect_uris = [
-    "https://argocd.mareo.fr/auth/callback"
+    "https://argocd.mareo.fr/auth/callback",
+    "http://localhost:8085/auth/callback",
   ]
   property_mappings = [
     data.authentik_scope_mapping.scope-openid.id,
@@ -32,6 +27,12 @@ resource "authentik_provider_oauth2" "argocd" {
     data.authentik_scope_mapping.scope-email.id,
   ]
   signing_key = data.authentik_certificate_key_pair.default.id
+
+  lifecycle {
+    ignore_changes = [
+      client_secret,
+    ]
+  }
 }
 
 resource "authentik_application" "argocd" {
