690ba35e72f6a91be074d1ec7d1575e20cb22e25
Marin Hannache <mareo@cri.epita.fr>
2022-06-13 08:30:25 +0200

    wip
    Signed-off-by: Marin Hannache <mareo@cri.epita.fr>

--- scripts/terraform_setup.sh
+++ scripts/terraform_setup.sh
@@ -6,11 +6,8 @@ BASEDIR="$(dirname "$0")/../"
 TERRAFORM_BUCKET="$(shyaml get-value terraform_bucket < "${BASEDIR}/config.yml")"
 S3_ENDPOINT_URL="$(shyaml get-value s3_endpoint_url < "${BASEDIR}/config.yml")"
 
-AWS_ACCESS_KEY_ID="$(shyaml get-value access_key < "${BASEDIR}/secrets/rgw_user_admin.yml")"
-AWS_SECRET_ACCESS_KEY="$(shyaml get-value secret_key < "${BASEDIR}/secrets/rgw_user_admin.yml")"
-export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
-
-aws s3api --endpoint-url="${S3_ENDPOINT_URL}" create-bucket --bucket "${TERRAFORM_BUCKET}" --acl private
+AWS_ACCESS_KEY_ID="$(shyaml get-value access_key < "${BASEDIR}/secrets/rgw_user_terraform.yml")"
+AWS_SECRET_ACCESS_KEY="$(shyaml get-value secret_key < "${BASEDIR}/secrets/rgw_user_terraform.yml")"
 
 cat > "${BASEDIR}/secrets/terraform_config" <<EOF
 region     = "ceph"
