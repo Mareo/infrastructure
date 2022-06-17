a7291d247b2f8e53ba106f77daec09400f9c0799
Marin Hannache <git@mareo.fr>
2022-06-17 20:24:49 +0200

    wip
    Signed-off-by: Marin Hannache <git@mareo.fr>

--- vault/k8s.mareo.fr/gitlab.tf
+++ vault/k8s.mareo.fr/gitlab.tf
@@ -25,3 +25,12 @@ resource "vault_generic_secret" "gitlab_redis-password" {
     password = random_password.gitlab_redis-password.result
   })
 }
+
+resource "vault_generic_secret" "gitlab_object-storage" {
+  path         = "k8s/gitlab/object-storage"
+  disable_read = true
+  data_json = jsonencode({
+    s3_access_key = try(yamldecode(file("../secrets/rgw_user_gitlab.yml")).access_key, "FIXME")
+    s3_secret_key = try(yamldecode(file("../secrets/rgw_user_gitlab.yml")).secret_key, "FIXME")
+  })
+}
