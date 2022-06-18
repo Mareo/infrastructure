572d45230d8e91f66f5655e9121613a449ba5bcd
Marin Hannache <git@mareo.fr>
2022-06-18 02:20:23 +0200

    wip
    Signed-off-by: Marin Hannache <git@mareo.fr>

--- proxmox/dns.tf
+++ proxmox/dns.tf
@@ -11,18 +11,31 @@ locals {
     "gitlab.mareo.fr.",
     "kas.mareo.fr.",
     "minio.mareo.fr.",
-    "pages.mareo.fr.",
-    "*.pages.mareo.fr.",
     "registry.mareo.fr.",
     "smartcard.mareo.fr.",
   ])
+
+  dns_a = {
+    # Gitlab
+    "pages.mareo.fr."   = "65.108.17.229"
+    "*.pages.mareo.fr." = "65.108.17.229"
+  }
 }
 
 resource "dns_cname_record" "dns" {
-  zone = "mareo.fr."
-  name = trimsuffix(each.key, ".mareo.fr.")
-
   for_each = local.dns_cnames
 
+  zone  = "mareo.fr."
+  name  = trimsuffix(each.key, ".mareo.fr.")
   cname = "athena.mareo.fr."
+}
+
+resource "dns_a_record_set" "dns" {
+  for_each = local.dns_a
+
+  zone = "mareo.fr."
+  name = trimsuffix(each.key, ".mareo.fr.")
+  addresses = [
+    each.value
+  ]
 }
