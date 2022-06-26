0235bdcb7f30929cbe4081882e660de204b6179f
Marin Hannache <mareo@cri.epita.fr>
2022-06-26 18:45:03 +0200

    wip
    Signed-off-by: Marin Hannache <mareo@cri.epita.fr>

--- authentik/gitlab.tf
+++ authentik/gitlab.tf
@@ -1,22 +1,22 @@
-resource "authentik_user" "authentik_mail" {
-  username = "authentik-mail"
-  name     = "authentik-mail"
+resource "authentik_user" "gitlab_mail" {
+  username = "gitlab-mail"
+  name     = "gitlab-mail"
   groups   = [authentik_group.groups["mail"].id]
   attributes = jsonencode({
     "goauthentik.io/user/service-account": true
   })
 }
 
-resource "authentik_token" "authentik_mail" {
-  identifier   = "authentik-mail"
-  user         = authentik_user.authentik_mail.id
+resource "authentik_token" "gitlab_mail" {
+  identifier   = "gitlab-mail"
+  user         = authentik_user.gitlab_mail.id
   intent       = "app_password"
   expiring     = false
   retrieve_key = true
 }
 
-resource "vault_generic_secret" "authentik_mail" {
-  path         = "k8s/authentik/mail"
+resource "vault_generic_secret" "gitlab_mail" {
+  path         = "k8s/gitlab/mail"
   disable_read = true
   data_json = jsonencode({
     username = authentik_user.authentik_mail.username
