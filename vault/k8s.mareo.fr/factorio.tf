76dd09a2166c2dcac4215ab85f9e7279d373f2df
Marin Hannache <mhannache@equativ.com>
2022-12-11 21:55:10 +0100

    Add factorio server
    Signed-off-by: Marin Hannache <mhannache@equativ.com>

---
+++ vault/k8s.mareo.fr/factorio.tf
@@ -0,0 +1,18 @@
+resource "random_password" "factorio_rcon-password" {
+  length  = 16
+  special = false
+}
+
+resource "vault_generic_secret" "factorio-server" {
+  path = "k8s/factorio/server"
+  data_json = jsonencode({
+    password = "FIXME"
+  })
+}
+
+resource "vault_generic_secret" "factorio-rcon" {
+  path = "k8s/factorio/rcon"
+  data_json = jsonencode({
+    password = random_password.factorio_rcon-password.result
+  })
+}
