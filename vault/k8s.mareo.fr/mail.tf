3bca7773570036c575b1bea54e10ed1c75103595
Marin Hannache <git@mareo.fr>
2023-07-16 14:19:44 +0200

    wip
    Signed-off-by: Marin Hannache <git@mareo.fr>

--- vault/k8s.mareo.fr/mail.tf
+++ vault/k8s.mareo.fr/mail.tf
@@ -1,3 +1,17 @@
+locals {
+  zones = {
+    "mareo.fr." = [
+      "athena",
+      "auth",
+      "gitlab",
+      "grafana",
+      "nextcloud",
+      "vaultwarden",
+    ]
+    "hannache.fr." = []
+  }
+}
+
 resource "random_password" "mail_postsrsd-secret" {
   length  = 18
   special = false
@@ -22,9 +36,34 @@ resource "vault_generic_secret" "mail_opendkim" {
   })
 }
 
-resource "dns_txt_record_set" "mail_dkim-athena" {
-  zone = "mareo.fr."
+resource "dns_txt_record_set" "mail_dkim-root" {
+  for_each = local.zones
+
+  zone = each.key
   name = "athena._domainkey"
+  txt = [
+    format(
+      "v=DKIM1; k=rsa; p=%s",
+      replace(
+        replace(
+          tls_private_key.mail_opendkim-athena.public_key_pem,
+          "/--*.*--*/",
+          ""
+        ),
+        "\n",
+        "",
+      )
+    )
+  ]
+}
+
+resource "dns_txt_record_set" "mail_dkim-subdomains" {
+  for_each = { for i, o in flatten([for zone, domains in local.zones :
+    [for domain in domains : { zone = zone, domain = domain }]
+  ]) : i => o }
+
+  zone = each.value.zone
+  name = "athena._domainkey.${each.value.domain}"
   txt = [
     format(
       "v=DKIM1; k=rsa; p=%s",
