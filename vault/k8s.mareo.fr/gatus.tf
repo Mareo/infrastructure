a4da6a8ea2aea47155b0e254dd946dcdc57ac5a9
Marin Hannache <git@mareo.fr>
2024-05-29 01:05:55 +0200

    k8s: gatus: add gitlab alerting support
    Signed-off-by: Marin Hannache <git@mareo.fr>

--- vault/k8s.mareo.fr/gatus.tf
+++ vault/k8s.mareo.fr/gatus.tf
@@ -2,6 +2,16 @@ resource "vault_generic_secret" "gatus_discord" {
   path         = "k8s/gatus/discord"
   disable_read = true
   data_json = jsonencode({
-    webhook = "FIXME"
+    DISCORD_WEBHOOK             = "FIXME"
+    DISCORD_WEBHOOK_PETITSTREAM = "FIXME"
+  })
+}
+
+resource "vault_generic_secret" "gatus_gitlab" {
+  path         = "k8s/gatus/gitlab"
+  disable_read = true
+  data_json = jsonencode({
+    GITLAB_WEBHOOK_URL       = "FIXME"
+    GITLAB_AUTHORIZATION_KEY = "FIXME"
   })
 }
